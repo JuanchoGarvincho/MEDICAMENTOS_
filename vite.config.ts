8b12e876b1a49754da668d9ae323b61638a4240f
diego <diegoalejandro619@gmail.com>
2026-03-30 21:48:40 -0500

    Fix Depliegue

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,16 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
+  base: command === 'build' ? '/MEDICAMENTOS_/' : '/',
   assetsInclude: ['**/*.svg', '**/*.csv'],
-})
+}))
